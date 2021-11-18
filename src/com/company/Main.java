d5788b4cfb1fc85c44124485faec221502ac55dd
mo212094 <mo212094@reigate.ac.uk>
2021-11-18 13:06:49 +0000

    my work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,61 @@
 package com.company;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class Main {
 
-    public static int mirrors() {
-
-    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        ArrayList<String> userNumbers = new ArrayList<String>();
-        while(true){
-            System.out.println( "Type done if finished");
-            String userInput = input.next();
-            if (userInput.equals("done")){
-                break;
-            }
-            userNumbers.add(userInput);
-        }
+        ArrayList<Integer> userNumbers = new ArrayList<Integer>();
+        ArrayList<Integer> reversedNumbers = new ArrayList<Integer>();
+
+        userNumbers = getNumbers();
+        reversedNumbers = reverseArray(userNumbers);
+
         System.out.println(userNumbers);
+
+        boolean mirror = isMirrored(userNumbers);
+        System.out.println(mirror);
+
     }
+
+    //
+    public static ArrayList<Integer> getNumbers(){
+        ArrayList<Integer> userNumbers = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        while(true){
+            try{
+                System.out.println( "Type done if finished");
+                String userInput = input.next();
+
+                if (userInput.equals("done")){
+                    break;
+                }
+                userNumbers.add(Integer.parseInt(userInput));
+            }catch(Exception e){
+                System.out.println();
+            }
+        }
+        return userNumbers;
+    }
+    //
+    public static ArrayList<Integer> reverseArray(ArrayList<Integer> userArray){
+        ArrayList<Integer> reversedArray = new ArrayList<>();
+        for(int i = userArray.size()-1; i >= 0; i--){
+            reversedArray.add(userArray.get(i));
+        }
+        return reversedArray;
+    }
+    //
+    public static boolean isMirrored(ArrayList<Integer> userArray){
+        int size = userArray.size();
+        Collections.sort(userArray);
+
+
+        return false;
+    }
+
 }
